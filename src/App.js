8bc96583790361edb4a08c2759ec72e65bbd5577
amarmohammed20 <63112654+amarmohammed20@users.noreply.github.com>
2022-07-25 13:56:50 +0100

    [update] useQuery Hook Used

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import GetLaunchesQuery from './graphqlQueries/GetLaunchesQuery';
 
 function App() {
+
+  const [showLaunches, setShowLaunches] = useState(false);
+  const [buttonValue, setButtonValue] = useState('Click Here To View All Missions')
+
+  const toggleMissionView = () => setShowLaunches(showLaunches => !showLaunches);
+
+
   return (
     <div className="App">
       <h1>Apollo Client Hooks Demo</h1>
+      <button onClick={toggleMissionView}>{buttonValue}</button>
+      { 
+        showLaunches ? 
+          <p><GetLaunchesQuery /></p> 
+          : 
+          <p>Nothing to see here if you dont press the button...</p>
+      }
     </div>
   );
 }
---
+++ src/graphqlQueries/GetLaunchesQuery.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useQuery, gql } from "@apollo/client"
+
+function GetLaunchesQuery() {
+  const launchesQuery = gql `
+    query GetLaunches {
+        launches {
+            id
+            launch_success
+            launch_year
+            mission_name
+        }
+    }`;
+
+  const { loading, error, data } = useQuery(launchesQuery);
+  if(loading) return <p>Loading.....</p>;
+  if(error) return <p>Error: {error}</p>;
+
+  // console.log(data);
+
+  return (
+    <div>
+      <h1>Launches Comp</h1>
+        {
+          data.launches.map((launch) => {
+            return (
+            <div key={launch.id}>
+            <p>Mission name - {launch.mission_name}</p>
+            </div>
+            )
+          })
+        }      
+    </div>
+  )
+}
+
+export default GetLaunchesQuery;
+
